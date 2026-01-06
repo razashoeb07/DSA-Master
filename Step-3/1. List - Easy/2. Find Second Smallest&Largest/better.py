66ee45d1b74b9d713d8f905921a3579cb929bf02
Shoeb Raza <razashoeb46@gmail.com>
2026-01-07 02:06:12 +0530

    Find Second Smallest&Largest: Better version

---
+++ Step-3/1. List - Easy/2. Find Second Smallest&Largest/better.py
@@ -0,0 +1,27 @@
+def second_small_large(nums):
+    n = len(nums)
+    smallest = nums[0]
+    s_smallest = float("inf")
+    largest = nums[0]
+    s_largest = float("-inf")
+
+    for i in range(n):
+        if nums[i] <= smallest:
+            smallest = nums[i]
+
+        if nums[i] >= largest:
+            largest = nums[i]
+
+    for i in range(n):
+        if nums[i] > smallest and nums[i] < s_smallest:
+            s_smallest = nums[i]
+
+        if nums[i] < largest and nums[i] > s_largest:
+            s_largest = nums[i]
+
+    return s_smallest,s_largest
+
+if __name__ == "__main__":
+    nums = [10,12,8,6,7,16]
+    ans = second_small_large(nums)
+    print(ans)
