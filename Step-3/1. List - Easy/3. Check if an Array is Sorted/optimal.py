bac5c32e390ff6ce7938ea4e87c3f400d529ed53
Shoeb Raza <razashoeb46@gmail.com>
2026-01-25 18:21:13 +0530

    Update

--- Step-3/1. List - Easy/3. Check if an Array is Sorted/optimal.py
+++ Step-3/1. List - Easy/3. Check if an Array is Sorted/optimal.py
@@ -1,12 +1,3 @@
-"""
-Example 1:
-Input: N = 5, array[] = {1,2,3,4,5}
-Output: True.
-Explanation: The given array is sorted i.e Every element in the array is smaller than or equals to
-its next values, So the answer is True.
-"""
-
-
 def isArraySorted(nums):
     n = len(nums)
     for i in range(n-1):
